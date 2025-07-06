fef228dd05a9205409218e85b66e18822adbe5d1
unknown <fareedzeeshan868@gmail.com>
2025-07-07 00:18:43 +0500

    progress

--- script.js
+++ script.js
@@ -291,15 +291,22 @@ async function displayAlbum() {
         const card = document.createElement("div");
         card.classList.add("card");
         card.dataset.folder = folder; // ⬅️ Use data-folder
-        card.innerHTML = `
+        card.innerHTML = ` 
           <div class="play">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="48" height="48">
-              <circle cx="24" cy="24" r="24" fill="green" />
-              <g transform="translate(12,12) scale(0.75)">
-                <path d="M12 20.5...Z" stroke="#000" stroke-width="1.5" fill="none"/>
-                <path d="M15.9621 12.31...Z" stroke="#000" stroke-width="1.5" fill="none" stroke-linejoin="round"/>
-              </g>
-            </svg>
+                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="48" height="48">
+                                <!-- Green circular background -->
+                                <circle cx="24" cy="24" r="24" fill="green" />
+
+                                <!-- Scaled-down and centered icon -->
+                                <g transform="translate(12,12) scale(0.75)">
+                                    <path
+                                        d="M12 20.5C13.8097 20.5 15.5451 20.3212 17.1534 19.9934C19.1623 19.5839 20.1668 19.3791 21.0834 18.2006C22 17.0221 22 15.6693 22 12.9635V11.0365C22 8.33073 22 6.97787 21.0834 5.79937C20.1668 4.62088 19.1623 4.41613 17.1534 4.00662C15.5451 3.67877 13.8097 3.5 12 3.5C10.1903 3.5 8.45489 3.67877 6.84656 4.00662C4.83766 4.41613 3.83321 4.62088 2.9166 5.79937C2 6.97787 2 8.33073 2 11.0365V12.9635C2 15.6693 2 17.0221 2.9166 18.2006C3.83321 19.3791 4.83766 19.5839 6.84656 19.9934C8.45489 20.3212 10.1903 20.5 12 20.5Z"
+                                        stroke="#000000" stroke-width="1.5" fill="none" />
+                                    <path
+                                        d="M15.9621 12.3129C15.8137 12.9187 15.0241 13.3538 13.4449 14.2241C11.7272 15.1705 10.8684 15.6438 10.1728 15.4615C9.9372 15.3997 9.7202 15.2911 9.53799 15.1438C9 14.7089 9 13.8059 9 12C9 10.1941 9 9.29112 9.53799 8.85618C9.7202 8.70886 9.9372 8.60029 10.1728 8.53854C10.8684 8.35621 11.7272 8.82945 13.4449 9.77593C15.0241 10.6462 15.8137 11.0813 15.9621 11.6871C16.0126 11.8933 16.0126 12.1067 15.9621 12.3129Z"
+                                        stroke="#000000" stroke-width="1.5" fill="none" stroke-linejoin="round" />
+                                </g>
+                            </svg>
           </div>
           <img src="/songs/${folder}/cover.jpeg" alt="${info.title}">
           <h2>${info.title}</h2>
@@ -412,5 +419,17 @@ document.querySelector(".songInfo").innerHTML=decodeURI(track);
 document.querySelector(".songTime").innerHTML="00:00 / 00:00";
 }
 
+document.querySelector(".volume img").addEventListener("click",(e)=>{
+  console.log(e.target)
 
-
+  if(e.target.src.includes("volume.svg")){
+    e.target.src= e.target.src.replace("volume.svg","mute.svg");
+    currentSong.volume=0;
+      document.querySelector(".range").getElementsByTagName("input")[0].value=0;
+  }
+  else{
+     currentSong.volume=.10;
+      e.target.src= e.target.src.replace("mute.svg","volume.svg");
+         document.querySelector(".range").getElementsByTagName("input")[0].value=10;
+  }
+})
